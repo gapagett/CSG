df9aea15fec45f621de1adf6afbe2d9e10211eaf
Greg Pagett <gapagett@cox.net>
2012-09-23 14:37:40 -0500

    Add user and password for Gmail accounts

--- CSGFileScan/CSGFileScan/Functions.vb
+++ CSGFileScan/CSGFileScan/Functions.vb
@@ -4,15 +4,19 @@ Module Functions
         Try
             Dim Server As New SmtpClient
             Dim NewMessage As New MailMessage
-            Server.Credentials = New Net.NetworkCredential("Casperpage", "Lily4_Frost")
+            'set the server credentials
+            Server.Credentials = New Net.NetworkCredential(InputBox("Enter GMail Login"), InputBox("Enter GMail Password (will not be saved)"))
+            'required in order to create a secure SSL. must set gmail to "Don't always use https"
+            Server.EnableSsl = True
             Server.Port = 587
             Server.Host = "smtp.gmail.com"
-            Server.EnableSsl = True
+
             NewMessage = New MailMessage
             NewMessage.From = New MailAddress("casperpage@gmail.com")
             NewMessage.To.Add("gapagett@cox.net")
             NewMessage.Subject = "CSG FileScan Errors"
             NewMessage.Body = "This is the body"
+            NewMessage.Priority = MailPriority.High
             Server.Send(NewMessage)
             MsgBox("Sent")
         Catch ex As Exception
